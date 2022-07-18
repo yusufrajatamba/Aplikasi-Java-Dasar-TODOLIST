edfdd34e4ad21ef6f434a063b58662931974262f
yusufrajatamba <yusufrajatamba@gmail.com>
2022-07-18 15:46:09 +0700

    Update

--- src/TodoListApp.java
+++ src/TodoListApp.java
@@ -4,12 +4,27 @@ public class TodoListApp {
 
     public static void main(String[] args) {
 
+        testShowTodoList();
     }
 
     //method menampilkan todolist
     public static void showTodoList(){
+        for(int i=0;i < model.length; i++){
+            var todo = model [i];
+            var no = i+1;
 
+            if(todo!=null){
+                System.out.println(no + "." +todo);
+            }
+        }
     }
+    //untuk melakukan test
+
+    public static void testShowTodoList (){
+        showTodoList();
+    }
+
+
 
     //method menambah todolist
     public static void addTodoList (){
@@ -21,6 +36,8 @@ public class TodoListApp {
     public static void removeTodoList(){
 
     }
+
+
     //---------------------------
 
     //Dibawah ini adalah view dari menampilkan todolist
