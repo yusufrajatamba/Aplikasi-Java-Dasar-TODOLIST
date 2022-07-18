4301b0dd8315a57310318c90466ecfcea7b908a4
yusufrajatamba <yusufrajatamba@gmail.com>
2022-07-18 15:27:03 +0700

    Update

---
+++ src/TodoListApp.java
@@ -0,0 +1,40 @@
+public class TodoListApp {
+
+    public static String [] model = new String[10];
+
+    public static void main(String[] args) {
+
+    }
+
+    //method menampilkan todolist
+    public static void showTodoList(){
+
+    }
+
+    //method menambah todolist
+    public static void addTodoList (){
+
+    }
+
+
+    //menghapus todoList
+    public static void removeTodoList(){
+
+    }
+    //---------------------------
+
+    //Dibawah ini adalah view dari menampilkan todolist
+    public static void viewShowTodoList (){
+
+    }
+
+    //Dibawah ini adalah view dari menambahkan todolist
+    public static void viewAddTodoList (){
+
+    }
+
+    //Dibawah ini adalah view dari menghapus todolist
+    public static void viewRemoveTodoList (){
+
+    }
+}
