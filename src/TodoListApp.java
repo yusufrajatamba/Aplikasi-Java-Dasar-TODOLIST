3983a30c0af39db9a25a6c68cf6e2a4141d97cc5
yusufrajatamba <yusufrajatamba@gmail.com>
2022-07-18 16:02:44 +0700

    Update

--- src/TodoListApp.java
+++ src/TodoListApp.java
@@ -14,21 +14,52 @@ public class TodoListApp {
             var no = i+1;
 
             if(todo!=null){
-                System.out.println(no + "." +todo);
+                System.out.println(no + ". " +todo);
             }
         }
     }
     //untuk melakukan test
 
     public static void testShowTodoList (){
+        model[0] = "belajar Java Dasar";
+        model[1] = "membuat Aplikasi TodoList";
         showTodoList();
     }
 
 
 
     //method menambah todolist
-    public static void addTodoList (){
+    public static void addTodoList (String addtodo){
 
+        var isFull = true;
+        for(int i=0 ; i< model.length;i++){
+            if(model[i]==null){
+                //model masih ada yang kosong
+                isFull = false;
+                break;
+            }
+        }
+
+        //jika penuh maka kita resize ukuran array dua kali lipat
+
+        if(isFull == true){
+            var temp = model;  //ini untuk menyimpan data lama ke sebuah variabel
+
+            model = new String[model.length*2]; // ini untuk resize panjang array "semua array akan di hilangkan
+
+            for (var i=0 ; i< temp.length;i++){
+                model [i] = temp [i];
+            }
+
+        }
+
+        //untuk menambahkan data ketodo
+        for(var i=0 ; i< model.length; i++){
+            if(model[i]==null){
+                model[i] = addtodo;
+                break;
+            }
+        }
     }
 
 
